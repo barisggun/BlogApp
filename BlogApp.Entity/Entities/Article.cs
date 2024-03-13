6bf73ef816682aa3ee8389ec074775db668e799b
Baris <konencally@gmail.com>
2024-03-13 17:12:48 +0300

    add article constructors

--- BlogApp.Entity/Entities/Article.cs
+++ BlogApp.Entity/Entities/Article.cs
@@ -4,6 +4,19 @@ namespace BlogApp.Entity.Entities;
 
 public class Article : EntityBase
 {
+    public Article()
+    {
+        
+    }
+    public Article(string title,string content, Guid userId, Guid categoryId, Guid imageId )
+    {
+        Title = title;
+        Content = content;
+        UserId = userId;
+        CategoryId = categoryId;
+        ImageId = imageId;
+        //ViewCount'ı burada da 0 verebiliriz. Ama aşağıda vermek daha performanslı.
+    }
     public string Title { get; set; }
     public string Content { get; set; }
     public int ViewCount { get; set; } = 0;
--- BlogApp.Web/obj/Debug/net8.0/BlogApp.Web.AssemblyInfo.cs
+++ BlogApp.Web/obj/Debug/net8.0/BlogApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10242fd96177f6a4c4c77187a33b039a1e52b044")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+459ad95f865a7301489dc268f04c2378d859e283")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
